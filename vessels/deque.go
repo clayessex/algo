6b4e58652f332c953595f469f4e3f9d2f00a63c0
Clayton Essex <clayessex@gmail.com>
2024-07-05 23:07:06 -0400

    deque doc

--- vessels/deque.go
+++ vessels/deque.go
@@ -1,18 +1,38 @@
+/**
+* Deque
+* Double ended queue implemented using a ring buffer
+* Holds (size - 1) items of T before the size is automatically doubled.
+ */
+
 package vessels
 
-/** Queue */
 const INITIAL_DEQUE_SIZE = 32
 
+/** Deque */
 type Deque[T any] struct {
+	/**
+	 * buf holds the deque items of T - len(buf) is also the
+	 * capacity of the Deque
+	 */
 	buf []T
 
-	/** head is the next avilable empty slot :: back/end */
+	/**
+	 * head is the next avilable empty slot :: back/end
+	 * PushBack/PopBack insert here
+	 */
 	head int
 
-	/** tail is the last value unless tail == head :: front/begin */
+	/**
+	 * tail is the last element unless tail == head :: front/begin
+	 * PushFront/PopFront insert before here
+	 */
 	tail int
 }
 
+/**
+ * Create a new Deque[T] sized to hold size-1 items
+ * before automatically resizing.
+ */
 func NewDeque[T any](size ...int) *Deque[T] {
 	sz := INITIAL_DEQUE_SIZE
 	if len(size) >= 1 {
@@ -25,6 +45,7 @@ func NewDeque[T any](size ...int) *Deque[T] {
 	}
 }
 
+/** copy the deque buffer into dst */
 func (d *Deque[T]) copy(dst []T) int {
 	sz := min(d.Len(), len(dst))
 	p := d.tail
@@ -69,6 +90,7 @@ func (d *Deque[T]) prev(index int) int {
 	return index - 1
 }
 
+/** length of the deque in use */
 func (d *Deque[T]) Len() int {
 	if d.tail <= d.head {
 		return d.head - d.tail
@@ -76,10 +98,12 @@ func (d *Deque[T]) Len() int {
 	return len(d.buf) - (d.tail - d.head)
 }
 
+/** capacity of the deque */
 func (d *Deque[T]) Cap() int {
 	return len(d.buf)
 }
 
+/** append to the end of the buffer */
 func (d *Deque[T]) PushBack(v T) {
 	if d.Len() == len(d.buf)-1 {
 		d.grow()
@@ -88,6 +112,7 @@ func (d *Deque[T]) PushBack(v T) {
 	d.head = d.next(d.head)
 }
 
+/** insert before the beginning of the buffer */
 func (d *Deque[T]) PushFront(v T) {
 	if d.Len() == len(d.buf)-1 {
 		d.grow()
@@ -96,6 +121,7 @@ func (d *Deque[T]) PushFront(v T) {
 	d.buf[d.tail] = v
 }
 
+/** remove and return the last element */
 func (d *Deque[T]) PopBack() T {
 	if d.Len() == 0 {
 		panic("can't PopBack() from an empty deque")
@@ -104,6 +130,7 @@ func (d *Deque[T]) PopBack() T {
 	return d.buf[d.head]
 }
 
+/** remove and return the first element */
 func (d *Deque[T]) PopFront() T {
 	if d.Len() == 0 {
 		panic("can't PopFront() from an empty deque")
@@ -113,15 +140,18 @@ func (d *Deque[T]) PopFront() T {
 	return result
 }
 
+/** remove all elements, leaving the deque empty */
 func (d *Deque[T]) Clear() {
 	d.tail = 0
 	d.head = 0
 }
 
+/** reduce the capacity by half unless the current elements won't fit */
 func (d *Deque[T]) Shrink() {
 	d.shrink()
 }
 
+/** create a clone of the deque */
 func (d *Deque[T]) Clone() *Deque[T] {
 	clone := &Deque[T]{
 		make([]T, len(d.buf)),

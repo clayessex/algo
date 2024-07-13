c9ea71b9dec8ce83dff857a3a9a073b87996c5cb
Clayton Essex <clayessex@gmail.com>
2024-07-13 17:06:45 -0400

    added list sort test fuzzing

--- vessels/list_test.go
+++ vessels/list_test.go
@@ -1,6 +1,8 @@
 package vessels
 
 import (
+	"cmp"
+	"fmt"
 	"testing"
 )
 
@@ -418,4 +420,69 @@ func TestListSortList(t *testing.T) {
 			expectOrdered(t, list.Begin(), list.End())
 		})
 	}
+}
+
+func isOrdered[T cmp.Ordered](first *ListNode[T], last *ListNode[T]) bool {
+	if first == last {
+		return true
+	}
+	first = first.next
+	for first != last {
+		if !(first.prev.value <= first.value) {
+			return false
+		}
+		first = first.next
+	}
+	return true
+}
+
+func intsToBytes(values []int) []byte {
+	result := make([]byte, len(values))
+	for i := 0; i < len(values); i++ {
+		result[i] = byte(values[i])
+	}
+	return result
+}
+
+func printBytes(values []byte) string {
+	result := "["
+	for i := 0; i < min(len(values), 100); i++ {
+		result = result + fmt.Sprintf("%v ", int(values[i]))
+	}
+	if len(values) > 100 {
+		result = result + "..."
+	}
+	result += "]"
+	return result
+}
+
+func FuzzListSortList(f *testing.F) {
+	f.Add([]byte{})
+	f.Add([]byte{1})
+	f.Add([]byte{2, 1})
+	f.Add([]byte{1, 2, 3, 4, 5, 6, 7, 8, 9})
+	f.Add([]byte{9, 8, 7, 6, 5, 4, 3, 2, 1})
+	f.Add([]byte{
+		249, 139, 93, 55, 25, 158, 198, 77, 67, 68, 255, 128, 135, 147, 198, 219,
+		59, 243, 210, 70, 112, 15, 68, 69, 39, 166, 132, 161, 233, 83, 166, 126,
+		74, 11, 129, 186, 26, 29, 149, 150, 247, 172, 99, 235, 12, 128, 131, 183,
+		82, 56, 221, 162, 211, 216, 91, 157, 214, 29, 195, 97, 189, 250, 205, 85,
+		78, 185, 60, 29, 160, 49, 16, 32, 253, 211, 141, 162, 251, 183, 200, 151,
+		142, 197, 197, 88, 49, 3, 210, 197, 92, 9, 251, 222, 108, 60, 234, 34,
+		25, 6, 142, 238,
+	})
+
+	f.Fuzz(func(t *testing.T, data []byte) {
+		list := NewList[byte]()
+		for _, v := range data {
+			list.Append(v)
+		}
+
+		SortList(list)
+
+		if !isOrdered(list.Begin(), list.End()) {
+			t.Log(data)
+			t.Fatal("sorted list is not sorted")
+		}
+	})
 }

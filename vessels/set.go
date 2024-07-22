671123dae749b728db235fe5861d446ef0389c12
Clayton Essex <clayessex@gmail.com>
2024-07-21 21:33:58 -0400

    add function doc and change 'keys' to 'elements'

--- vessels/set.go
+++ vessels/set.go
@@ -4,97 +4,114 @@ import "maps"
 
 type Set[T comparable] map[T]struct{}
 
-func NewSet[T comparable](keys ...T) Set[T] {
-	s := make(Set[T], len(keys))
-	for _, k := range keys {
+// Create a new Set of type T and (optionally) fill it with the given elements
+func NewSet[T comparable](elements ...T) Set[T] {
+	s := make(Set[T], len(elements))
+	for _, k := range elements {
 		s[k] = struct{}{}
 	}
 	return s
 }
 
-func (s Set[T]) contains(k T) bool {
-	_, ok := s[k]
+// Set contains el
+func (s Set[T]) contains(el T) bool {
+	_, ok := s[el]
 	return ok
 }
 
-func (s Set[T]) missing(k T) bool {
-	_, ok := s[k]
+// Set does not contain el
+func (s Set[T]) missing(el T) bool {
+	_, ok := s[el]
 	return !ok
 }
 
+// Number of elements in the Set
 func (s Set[T]) Len() int {
 	return len(s)
 }
 
-func (s Set[T]) Contains(k T) bool {
-	return s.contains(k)
+// True if the Set contains el
+func (s Set[T]) Contains(el T) bool {
+	return s.contains(el)
 }
 
-func (s Set[T]) ContainsAll(keys ...T) bool {
-	for _, k := range keys {
-		if s.missing(k) {
+// True if the Set contains each of the given elements
+func (s Set[T]) ContainsAll(elements ...T) bool {
+	for _, el := range elements {
+		if s.missing(el) {
 			return false
 		}
 	}
 	return true
 }
 
-func (s Set[T]) ContainsAny(keys ...T) bool {
-	for _, k := range keys {
-		if s.contains(k) {
+// True if the Set contains any of the given elements
+func (s Set[T]) ContainsAny(elements ...T) bool {
+	for _, el := range elements {
+		if s.contains(el) {
 			return true
 		}
 	}
 	return false
 }
 
-func (s Set[T]) Add(k T) {
-	s[k] = struct{}{}
+// Add an element to the Set
+func (s Set[T]) Add(el T) {
+	s[el] = struct{}{}
 }
 
-func (s Set[T]) Append(keys ...T) {
-	for _, k := range keys {
-		s[k] = struct{}{}
+// Add several elements to the Set
+func (s Set[T]) Append(elements ...T) {
+	for _, el := range elements {
+		s[el] = struct{}{}
 	}
 }
 
-func (s Set[T]) Delete(k T) {
-	delete(s, k)
+// Remove an element from the Set
+func (s Set[T]) Delete(el T) {
+	delete(s, el)
 }
 
+// Remove all elements from the Set
 func (s Set[T]) Clear() {
 	clear(s)
 }
 
+// Create a slice of type T containing all of the elements in the Set
 func (s Set[T]) Keys() []T {
 	result := []T{}
-	for k := range s {
-		result = append(result, k)
+	for el := range s {
+		result = append(result, el)
 	}
 	return result
 }
 
+// Create a slice of type T containing all of the elements in the Set
+// Alias for Keys()
 func (s Set[T]) Values() []T {
 	return s.Keys()
 }
 
+// Compare the elements in the Set to those of parameter 'o' and return true if
+// both Sets are of equal length and contain all of the same elements
 func (s Set[T]) Equal(o Set[T]) bool {
 	if len(s) != len(o) {
 		return false
 	}
-	for k := range s {
-		if o.missing(k) {
+	for el := range s {
+		if o.missing(el) {
 			return false
 		}
 	}
 	return true
 }
 
+// Return a clone of the Set
 func (s Set[T]) Clone() Set[T] {
 	return maps.Clone(s)
 }
 
-// a or b or both
+// Return a new Set containing the elements in either Set a or Set b or both
 func SetUnion[T comparable](a, b Set[T]) Set[T] {
 	r := make(map[T]struct{}, len(a)+len(b))
 	maps.Copy(r, a)
@@ -102,49 +119,50 @@ func SetUnion[T comparable](a, b Set[T]) Set[T] {
 	return r
 }
 
-// a and b
+// Return a new Set containing the elements in both Set a and Set b
 func SetIntersection[T comparable](a, b Set[T]) Set[T] {
 	r := NewSet[T]()
 	if b.Len() < a.Len() {
 		b, a = a, b // opt: fewer comparisons
 	}
-	for k := range a {
-		if b.contains(k) {
-			r[k] = struct{}{}
+	for el := range a {
+		if b.contains(el) {
+			r[el] = struct{}{}
 		}
 	}
 	return r
 }
 
-// from a but not in b
+// Return a new Set containing the elements from Set a that are not in Set b
 func SetDifference[T comparable](a, b Set[T]) Set[T] {
 	r := NewSet[T]()
-	for k := range a {
-		if b.missing(k) {
-			r[k] = struct{}{}
+	for el := range a {
+		if b.missing(el) {
+			r[el] = struct{}{}
 		}
 	}
 	return r
 }
 
-// from a or b but not both
+// Return a new Set containing the elements from Set a or Set b, but not both
 func SetSymmetricDifference[T comparable](a, b Set[T]) Set[T] {
 	r := NewSet[T]()
-	for k := range a {
-		if b.missing(k) {
-			r[k] = struct{}{}
+	for el := range a {
+		if b.missing(el) {
+			r[el] = struct{}{}
 		}
 	}
-	for k := range b {
-		if a.missing(k) {
-			r[k] = struct{}{}
+	for el := range b {
+		if a.missing(el) {
+			r[el] = struct{}{}
 		}
 	}
 	return r
 }
 
+// Run the function f against each element of the Set
 func (s Set[T]) ForEach(f func(T)) {
-	for k := range s {
-		f(k)
+	for el := range s {
+		f(el)
 	}
 }
